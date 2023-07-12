7deae325949f10f54d070caf5fbe38ee13a21b83
Christoph Salvisberg <christoph.salvisberg@zuehlke.com>
2023-07-12 21:14:51 +0200

    Add Pages, Routing and show mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import { Layout } from "./components/layout/Layout";
-import { PokeList } from "./components/poke-list/PokeList";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import DetailPage from "./pages/details/DetailPage";
+import ListPage from "./pages/list/ListPage";
 import "./styles/global.scss";
+import { Layout } from "./components/layout/Layout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/pokemon",
+        element: <ListPage />,
+      },
+      {
+        path: "/pokemon/:pokemonName",
+        element: <DetailPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <Layout>
-      <PokeList />
-    </Layout>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/poke-list-entry/PokeListEntry.tsx
+++ src/components/poke-list-entry/PokeListEntry.tsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom";
+
 type PokeListEntryProps = {
   name?: string;
 };
 export function PokeListEntry({ name = "Bulbasaur" }: PokeListEntryProps) {
-  return <li>{name}</li>;
+  return (
+    <li>
+      <Link to={`./${name}`}>{name}</Link>
+    </li>
+  );
 }
