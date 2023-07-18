284f6d595d843863e0dfdb20ca95cdecc9787e11
Christoph Salvisberg <christoph.salvisberg@zuehlke.com>
2023-07-18 21:14:45 +0200

    Extend fetcher to allow non-fetch operations

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -1,5 +1,5 @@
-export async function fetcher<T>(uri: string): Promise<T> {
-  const response = await fetch(uri);
+export async function fetcher<T>(uri: string, init?: RequestInit): Promise<T> {
+  const response = await fetch(uri, init);
   if (!response.ok) throw new Error("Could not fetch data!");
   return response.json();
 }
