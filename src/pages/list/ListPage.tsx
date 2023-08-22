da80435520a1d443c47cd46bb9ad6ea71821e259
Christoph Salvisberg <christoph.salvisberg@zuehlke.com>
2023-08-22 20:46:56 +0200

    Add debounced filter on search result

--- src/pages/list/ListPage.tsx
+++ src/pages/list/ListPage.tsx
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import { fetcher } from "../../api/fetcher";
 import { PokemonListDto } from "../../api/pokeApi";
 import { PokeList } from "../../components/poke-list/PokeList";
+import { useEffect, useState } from "react";
+import { SearchPanel } from "../../components/search-panel/SearchPanel";
 
 function ListPage() {
   const { data, isLoading, isError } = useQuery({
@@ -9,10 +11,27 @@ function ListPage() {
     queryFn: () => fetcher<PokemonListDto>("https://pokeapi.co/api/v2/pokemon"),
   });
 
+  const [searchTerm, setSearchTerm] = useState("");
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => setDebouncedSearchTerm(searchTerm), 300);
+    return () => clearTimeout(timeoutId);
+  }, [searchTerm]);
+
   if (isLoading) return <div>LOADING</div>;
   if (isError) return <div>ERROR while loading data</div>;
 
-  return <PokeList pokemons={data?.results ?? []} />;
+  return (
+    <>
+      <SearchPanel searchTerm={searchTerm} onSearchChanged={setSearchTerm} />
+      <PokeList
+        pokemons={(data?.results ?? []).filter((p) =>
+          p.name.includes(debouncedSearchTerm)
+        )}
+      />
+    </>
+  );
 }
 
 export { ListPage };
