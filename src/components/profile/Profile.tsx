38acaee9eb79b52b2e63c7b6caa82b068304cc82
Christoph Salvisberg <christoph.salvisberg@zuehlke.com>
2023-08-22 20:33:58 +0200

    Add profile form implementation

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,20 +1,42 @@
-import { useState } from "react";
-import styles from "./Profile.module.scss";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type FormValues = { name: string; email: string };
 
 export function Profile() {
-  const [username, setUsername] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = (data) => console.log(data);
 
   return (
-    <>
-      <label htmlFor="username">Username</label>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <input
         type="text"
-        id="username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Dr. React"
+        {...register("name", {
+          required: { value: true, message: "Name is required!" },
+          maxLength: {
+            value: 25,
+            message: "The name should be shorter than 25 characters!",
+          },
+        })}
       />
-
-      <span className={styles.output}>Hello, ${username}</span>
-    </>
+      {errors?.name?.type && <span>{errors.name.message}</span>}
+      <input
+        type="text"
+        placeholder="react@zuhlke.com"
+        {...register("email", {
+          required: { value: true, message: "The 'email' field is required!" },
+          pattern: {
+            value: /^[A-Z0-9._+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+            message: "Invalid email!",
+          },
+        })}
+      />
+      {errors?.email?.type && <span>{errors.email.message}</span>}
+      <input type="submit" value="save" />
+    </form>
   );
 }
