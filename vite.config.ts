dd0cb77fb65dd63726b27ced2ff9454376aba57b
Christoph Salvisberg <christoph.salvisberg@zuehlke.com>
2023-12-29 14:36:25 +0100

    Fix vitest definition

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from "vitest/config";
+/// <reference types="vitest" />
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
