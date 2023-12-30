1adc706cbfd8c75409ba3fe19b06cde02105b713
Christoph Salvisberg <christoph.salvisberg@zuehlke.com>
2023-12-30 11:22:24 +0100

    Remove old exercise code

--- src/pages/list/ListPage.tsx
+++ src/pages/list/ListPage.tsx
@@ -2,8 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import { fetcher } from "../../api/fetcher";
 import { PokemonResultDto } from "../../api/pokeApi";
 import { PokeList } from "../../components/poke-list/PokeList";
-import { useDebouncedSearch } from "./useDebouncedSearch";
-import { SearchPanel } from "../../components/search-panel/SearchPanel";
 
 function ListPage() {
   const { data, isLoading, isError } = useQuery({
@@ -12,22 +10,10 @@ function ListPage() {
       fetcher<PokemonResultDto>("https://pokeapi.co/api/v2/pokemon"),
   });
 
-  const { searchTerm, setSearchTerm, debouncedSearchTerm } =
-    useDebouncedSearch("");
-
   if (isLoading) return <div>LOADING</div>;
   if (isError) return <div>ERROR while loading data</div>;
 
-  return (
-    <>
-      <SearchPanel searchTerm={searchTerm} onSearchChanged={setSearchTerm} />
-      <PokeList
-        pokemons={(data?.results ?? []).filter((p) =>
-          p.name.includes(debouncedSearchTerm)
-        )}
-      />
-    </>
-  );
+  return <PokeList pokemons={data?.results ?? []} />;
 }
 
 export { ListPage };
