9921b35f9919813e328658b097abfeda829c6b84
Felix Mokross <felix.mokross@zuehlke.com>
2025-02-07 16:09:33 +0100

    Chapter 5: Routing

---
+++ src/mockData/list.ts
@@ -0,0 +1,11 @@
+const pokemonList = {
+  count: 1281,
+  results: [
+    { name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/" },
+    { name: "ivysaur", url: "https://pokeapi.co/api/v2/pokemon/2/" },
+    { name: "venusaur", url: "https://pokeapi.co/api/v2/pokemon/3/" },
+    { name: "charmander", url: "https://pokeapi.co/api/v2/pokemon/4/" },
+    { name: "charmeleon", url: "https://pokeapi.co/api/v2/pokemon/5/" },
+  ],
+};
+export { pokemonList };
