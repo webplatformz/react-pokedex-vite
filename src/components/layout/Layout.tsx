736e19ebb000e8978048435fe0a70a8f04bcc5be
Christoph Salvisberg <christoph.salvisberg@zuehlke.com>
2025-11-05 21:14:38 +0100

    Use proper types for layout loaderData

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,15 +1,14 @@
-import { Link, Outlet, useLoaderData } from "react-router";
+import { Link, Outlet } from "react-router";
 import styles from "./Layout.module.scss";
 import { VisitCounter } from "./VisitCounter";
 import { profileService } from "../../db/profileService";
+import type { Route } from "./+types/Layout";
 
 export const loader = () => {
   return profileService.getProfile();
 };
 
-function Layout() {
-  const profile = useLoaderData<typeof loader>();
-
+function Layout({ loaderData }: Route.ComponentProps) {
   return (
     <div className={styles.root}>
       <header>
@@ -17,7 +16,7 @@ function Layout() {
           <Link to="/pokemon">Home</Link> | <Link to="/profile">Profile</Link>
         </nav>
         <VisitCounter />
-        <span>{profile?.name && `Hello, ${profile.name}`}</span>
+        <span>{loaderData?.name && `Hello, ${loaderData.name}`}</span>
       </header>
       <main>
         <Outlet />
